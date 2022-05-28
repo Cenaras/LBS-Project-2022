8cf67cf641fd0908fc4767b2e2cf33f5481a5b3f
Kasper S. Nielsen (desktop) <au617086@uni.au.dk>
2022-05-28 13:48:01 +0200

    only change level of people below your

--- blockchain_demo/lattice_contract_macro/src/lib.rs
+++ blockchain_demo/lattice_contract_macro/src/lib.rs
@@ -29,15 +29,17 @@ macro_rules! gen_lattice_contract {
                 target_level: &LatticeElement,
             ) {
                 let elements: Vec<String> = $lattice_elements;
-                let bot = String::from("bot");
-                let caller_level = self.map.get(caller_address).unwrap_or(&bot);
+                let caller_level = &self.get_level(caller_address);
 
                 // Only correct lattice elements can be used in map
                 assert!(elements.contains(target_level));
 
                 // Ensure that caller has appropiate level to perform set_level
                 assert!(self.flows_to($set_level, caller_level));
-                // Ensure that you cannot raise up to your own level
+                // Ensure that you can only change person with level under yours
+                let prev_level = &self.get_level(address);
+                assert!(self.flows_to(prev_level, caller_level) && prev_level.ne(caller_level));
+                // Ensure that you can only raise up to a level under your own level
                 assert!(self.flows_to(target_level, caller_level) && target_level.ne(caller_level));
 
                 self.map
